be04bc127108af275fcb2b7a7af6d956bdf40aed
Monal Aditya <119096811+MONAL7634@users.noreply.github.com>
2024-04-29 23:33:19 +0530

    first commit

---
+++ frontend/src/screens/ProductScreen.js
@@ -0,0 +1,159 @@
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useParams, Link, useNavigate } from "react-router-dom";
+import {
+  Row,
+  Col,
+  Image,
+  ListGroup,
+  Button,
+  Card,
+  FormControl,
+  Form,
+} from "react-bootstrap";
+import { listProductDetails } from "../redux/actions/productActions";
+import Loading from "../components/Loading";
+import Message from "../components/Message";
+import products from "../products";
+import Rating from "../components/Rating";
+import { addToCart } from "../redux/actions/cartActions";
+import { createProductReview } from "../redux/actions/productActions";
+
+const ProductScreen = () => {
+  const [qty, setQty] = useState(1);
+  const [rating, setRating] = useState(0);
+  const [comment, setComment] = useState("");
+  let navigate = useNavigate();
+  const dispatch = useDispatch();
+  let params = useParams();
+
+  const productDetails = useSelector((state) => state.productDetails);
+  const { product, loading, error } = productDetails;
+
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+
+  const productReviewCreate = useSelector((state) => state.productReviewCreate);
+  const {
+    loading: loadingProductReview,
+    success: successProductReview,
+    error: errorProductReview,
+  } = productReviewCreate;
+
+  useEffect(() => {
+    if (successProductReview) {
+      setRating(0);
+      setComment("");
+    }
+    if (error) {
+      navigate("/");
+    }
+    dispatch(listProductDetails(params.id));
+  }, [dispatch, params.id, successProductReview]);
+
+  const addToCartHandler = () => {
+    dispatch(addToCart(params.id, qty));
+    navigate(`/cart`);
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(createProductReview(params.id, { rating, comment }));
+  };
+
+  return (
+    <>
+      {loading ? (
+        <Loading />
+      ) : error ? (
+        <Message variant="danger"></Message>
+      ) : (
+        product && (
+          <>
+            <Link className="btn btn-light my-3" to="/">
+              Go back
+            </Link>
+            <Row>
+              <Col md={4}>
+                <Image src={product.image} alt={product.name} fluid />
+              </Col>
+              <Col md={5}>
+                <h3>{product.name}</h3>
+                <ListGroup variant="flush">
+                  <ListGroup.Item>
+                    <Rating
+                      value={product.rating}
+                      text={`${product.rating} Reviews`}
+                    />
+                  </ListGroup.Item>
+                  <ListGroup.Item>Price: Rs.{product.price}</ListGroup.Item>
+                  <ListGroup.Item>
+                    Description: {product.description}
+                  </ListGroup.Item>
+                </ListGroup>
+              </Col>
+              <Col md={3}>
+                <Card>
+                  <ListGroup variant="flush">
+                    <ListGroup.Item>
+                      <Row>
+                        <Col>Price:</Col>
+                        <Col>
+                          <strong>Rs.{product.price}</strong>
+                        </Col>
+                      </Row>
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                      <Row>
+                        <Col>Status:</Col>
+                        <Col>
+                          {product.countInStock > 0
+                            ? `In stock`
+                            : `Out of stock`}
+                        </Col>
+                      </Row>
+                    </ListGroup.Item>
+                    {product.countInStock > 0 && (
+                      <ListGroup.Item>
+                        <Row>
+                          <Col>Qty</Col>
+                          <Col>
+                            <FormControl
+                              as="select"
+                              value={qty}
+                              onChange={(e) => setQty(e.target.value)}
+                            >
+                              {[...Array(product.countInStock).keys()].map(
+                                (x) => (
+                                  <option value={x + 1} key={x + 1}>
+                                    {x + 1}
+                                  </option>
+                                )
+                              )}
+                            </FormControl>
+                          </Col>
+                        </Row>
+                      </ListGroup.Item>
+                    )}
+                    <ListGroup.Item>
+                      <Button
+                        onClick={addToCartHandler}
+                        className="btn btn-primary d-block w-100"
+                        type="button"
+                        disabled={product.countInStock === 0}
+                      >
+                        Add to cart
+                      </Button>
+                    </ListGroup.Item>
+                  </ListGroup>
+                </Card>
+              </Col>
+            </Row>
+          </>
+        )
+      )}
+    </>
+  );
+};
+
+export default ProductScreen;
